226bd403025c70a42e82b4bc502d009c042115e9
vaishnavi.pawar <vaishnavi.pawar@servicenow.com>
2024-05-30 09:34:01 +0530

    "added NavBar"

---
+++ src/frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+                <div className="container-fluid px-7">
+                <a className="navbar-brand" href="/">TimeBuddy</a>
+                <div className="d-flex">
+                    <button type="button" class="btn btn-secondary me-2">Sign-In</button>
+                    <button type="button" class="btn btn-secondary">Register</button>
+                </div>
+                </div>
+            </nav>
+        </div> 
+  )
+}
+
+export default NavBar
--- src/frontend/src/pages/Home/Home.jsx
+++ src/frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Search from '../../components/Search/Search'
 import './Home.css'
+import NavBar from '../../components/NavBar/NavBar'
 
 
 
 function Home() {
   return (
     <div>
-    <h3>YOUR TIME BUDDY</h3>
-    <Search /> 
+        <NavBar/>
+        <div className="Search_container">
+        <h3>YOUR TIME BUDDY</h3>
+        <Search /> 
+        </div>
     </div>
   )
 }
